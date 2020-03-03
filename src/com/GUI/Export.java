a8d11032972bc836ab2f2d159e63c031d2ceaf25
tiwarivarun <tiwarivarun@users.noreply.github.com>
2020-03-03 00:13:26 +0000

    Export complete

--- src/com/GUI/Export.java
+++ src/com/GUI/Export.java
@@ -9,13 +9,22 @@ import javax.swing.BorderFactory;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Export {
 
 	JFrame frame;
-	private JTextField textField;
 	private JTextField textField_1;
 
 	/**
@@ -46,22 +55,88 @@ public class Export {
 		frame.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(3, 2));
 		
-		JButton button = new JButton("NumLecturesForDepartment:");
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select Gender", "Male", "Female", "Rather not say"}));
+		panel.add(comboBox);
+		
+		
+		
+		JButton button = new JButton("Details by Donor's Gender");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = DBConnection.initiate_db_conn();
+				String insertQuery = "select * from donor where gender ='"  +comboBox.getSelectedItem().toString()+"';";
+				try {
+					PreparedStatement pstmt = con.prepareStatement(insertQuery);
+					ResultSet rs = pstmt.executeQuery();
+					writeToFile(rs,"Details_by_Donor_Name.csv");
+					rs.close();
+					pstmt.close();
+				}catch (Exception e1) {
+					System.out.println("fail "+e1);
+				}
+			}
+		});
 		panel.add(button);
 		
-		textField = new JTextField(12);
-		panel.add(textField);
-		
-		JButton button_1 = new JButton("AvgAgeForDepartment");
-		panel.add(button_1);
 		
 		textField_1 = new JTextField(12);
 		panel.add(textField_1);
 		
-		JButton button_2 = new JButton("ListAllDepartments");
+		JButton button_1 = new JButton("Details By Blood Group");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = DBConnection.initiate_db_conn();
+				String insertQuery = "select * from donor where bgroup ='"  +textField_1.getText()+"';";
+				try {
+					PreparedStatement pstmt = con.prepareStatement(insertQuery);
+					ResultSet rs = pstmt.executeQuery();
+					writeToFile(rs,"Details_By_Blood_Group.csv");
+					rs.close();
+					pstmt.close();
+				}catch (Exception e1) {
+					System.out.println("fail "+e1);
+				}
+			}
+		});
+		panel.add(button_1);
+		
+		
+		
+		JButton button_2 = new JButton("All Donor Names");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = DBConnection.initiate_db_conn();
+				String insertQuery = "Select fName from donor";
+				try {
+					PreparedStatement pstmt = con.prepareStatement(insertQuery);
+					ResultSet rs = pstmt.executeQuery();
+					writeToFile(rs,"All_Donor_Names.csv");
+					rs.close();
+					pstmt.close();
+				}catch (Exception e1) {
+					System.out.println("fail "+e1);
+				}
+			}
+		});
 		panel.add(button_2);
 		
-		JButton button_3 = new JButton("ListAllPositions");
+		JButton button_3 = new JButton("Blood Groups Available");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Connection con = DBConnection.initiate_db_conn();
+				String insertQuery = "select bgroup,count(bgroup) as Available from donor group by bgroup";
+				try {
+					PreparedStatement pstmt = con.prepareStatement(insertQuery);
+					ResultSet rs = pstmt.executeQuery();
+					writeToFile(rs,"Blood_Groups_Available.csv");
+					rs.close();
+					pstmt.close();
+				}catch (Exception e1) {
+					System.out.println("fail "+e1);
+				}
+			}
+		});
 		panel.add(button_3);
 		
 		JButton btnNewButton = new JButton("Cancel");
@@ -74,5 +149,29 @@ public class Export {
 		});
 		btnNewButton.setBounds(434, 239, 89, 23);
 		frame.getContentPane().add(btnNewButton);
+	}
+
+	public void writeToFile(ResultSet rs,String fileName){
+		try{
+			System.out.println("All of the data is saved in Blood_Donor.csv file");
+			FileWriter outputFile = new FileWriter(fileName);
+			PrintWriter printWriter = new PrintWriter(outputFile);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numColumns = rsmd.getColumnCount();
+
+			for(int i=0;i<numColumns;i++){
+				printWriter.print(rsmd.getColumnLabel(i+1)+",");
+			}
+			printWriter.print("\n");
+			while(rs.next()){
+				for(int i=0;i<numColumns;i++){
+					printWriter.print(rs.getString(i+1)+",");
+				}
+				printWriter.print("\n");
+				printWriter.flush();
+			}
+			printWriter.close();
+		}
+		catch(Exception e){e.printStackTrace();}
 	}
 }
--- src/com/GUI/Search_Name.java
+++ src/com/GUI/Search_Name.java
@@ -30,7 +30,6 @@ public class Search_Name {
 	 * Launch the application.
 	 */
 
-
 	/**
 	 * Create the application.
 	 */
@@ -46,68 +45,66 @@ public class Search_Name {
 		frame.setBounds(100, 100, 509, 333);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 56, 469, 226);
 		frame.getContentPane().add(scrollPane);
-		
+
 		table = new JTable();
 
 		scrollPane.setViewportView(table);
 		table.setBackground(Color.PINK);
-		
+
 		JLabel lblDonorName = new JLabel("Donor Name");
 		lblDonorName.setBounds(21, 22, 76, 14);
 		frame.getContentPane().add(lblDonorName);
-		
+
 		name = new JTextField();
 		name.setColumns(10);
 		name.setBounds(101, 19, 182, 20);
 		frame.getContentPane().add(name);
-		
+
 		JButton button = new JButton("Find");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Connection con = DBConnection.initiate_db_conn();
-				String insertQuery ="Select * from donor where fName = ?;";	
-						try {
-							String[] tableColumnsName = {"Id","Name","Blood Group","Gender","Age"}; 
-							DefaultTableModel aModel = (DefaultTableModel) table.getModel();
-							aModel.setColumnIdentifiers(tableColumnsName);
-							PreparedStatement pstmt = con.prepareStatement(insertQuery);
-							pstmt.setString(1, name.getText());
-							ResultSet rs= pstmt.executeQuery();
-							java.sql.ResultSetMetaData rsmd = rs.getMetaData();
-							int colNo = rsmd.getColumnCount();
-							while(rs.next()){
-							 Object[] objects = new Object[colNo];
-							 // tanks to umit ozkan for the bug fix!
-							 for(int i=0;i<colNo;i++){
-							  objects[i]=rs.getObject(i+1);
-							  }
-							 aModel.addRow(objects);
-							}
-							table.setModel(aModel);
-							rs.close();
-							pstmt.close();
-							System.out.println("success");
+				String insertQuery = "Select * from donor where fName = ?;";
+				try {
+					String[] tableColumnsName = { "Id", "Name", "Blood Group", "Gender", "Age" };
+					DefaultTableModel aModel = (DefaultTableModel) table.getModel();
+					aModel.setColumnIdentifiers(tableColumnsName);
+					PreparedStatement pstmt = con.prepareStatement(insertQuery);
+					pstmt.setString(1, name.getText());
+					ResultSet rs = pstmt.executeQuery();
+					java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+					int colNo = rsmd.getColumnCount();
+					while (rs.next()) {
+						Object[] objects = new Object[colNo];
+						// tanks to umit ozkan for the bug fix!
+						for (int i = 0; i < colNo; i++) {
+							objects[i] = rs.getObject(i + 1);
 						}
-				catch (SQLException sqle)
-				{
-					System.err.println("Error with  insert:\n"+sqle.toString());
+						aModel.addRow(objects);
+					}
+					table.setModel(aModel);
+					rs.close();
+					pstmt.close();
+					System.out.println("success");
+				} catch (SQLException sqle) {
+					System.err.println("Error with  insert:\n" + sqle.toString());
 				}
-				
+
 			}
 		});
 		button.setBounds(293, 18, 89, 23);
 		frame.getContentPane().add(button);
-		
+
 		btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
-		        Search_Select b = new Search_Select();
-		        b.frame.setVisible(true);
+				Search_Select b = new Search_Select();
+				b.frame.setVisible(true);
 			}
 		});
 		btnCancel.setBounds(390, 18, 89, 23);
